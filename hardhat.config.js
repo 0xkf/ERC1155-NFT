0df1f24d61d9877e1604b76d969e6b6d1e6479f8
0xkf <Kanichi73@me.com>
2023-01-10 11:56:14 +0900

    first commit

---
+++ hardhat.config.js
@@ -0,0 +1,22 @@
+// require("@nomicfoundation/hardhat-toolbox");
+
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.17",
+// };
+
+require('dotenv').config();
+require("@nomicfoundation/hardhat-toolbox");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+module.exports = {
+   solidity: "0.8.4",
+   defaultNetwork: "goerli",
+   networks: {
+      hardhat: {},
+      goerli: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+   },
+}
