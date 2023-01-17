c96394ebf1d03e7af9b17d36215741ed812c789b
0xkf <Kanichi73@me.com>
2023-01-17 17:16:34 +0900

    added memo

--- contracts/AwesomeGame.sol
+++ contracts/AwesomeGame.sol
@@ -18,3 +18,58 @@ contract AwesomeGame is ERC1155 {
         _mint(msg.sender, CROWN, 1, "");
     }
 }
+
+
+
+
+// memo
+
+// 初期設定？
+//     uint256 public tokenCount;
+//     uint256 public batchSize = 1000;
+    // uint256 public _totalSupply = 10000;
+
+//       constructor(address _manager ) ERC721A("sineMC", "sineMC") {
+//       manager = _manager;
+//       tokenCount = 0;
+//   }
+
+// uint256 public ownerLimit = 1000; // 1000
+//   // owner mint
+//   function ownerMint(uint256 quantity, address to) external onlyOwnerOrManager {
+//     require((quantity + tokenCount) <= (_totalSupply), "too many already minted before patner mint");
+//     require((quantity + tokenCount) <= (ownerLimit), "too many already minted before patner mint");
+//     _safeMint(to, quantity);
+//     tokenCount += quantity;
+//   }
+
+
+//   string public baseURI;
+// string public unrevealedTokenUri;
+
+
+// ==================tokenURI,BaseURI====================
+//   //retuen BaseURI.internal.
+//   function _baseURI() internal view override returns (string memory){
+//     return baseURI;
+//   }
+
+// function tokenURI(uint256 _tokenId) public view virtual override(ERC721A, IERC721A) returns (string memory) {
+//     require(_exists(_tokenId), "URI query for nonexistent token");
+//     if(revealed == false) {
+//       return unrevealedTokenUri;
+//     }
+//     return string(abi.encodePacked(_baseURI(), Strings.toString(_tokenId), ext));
+//   }
+// ==================
+
+//  //set URI
+//   function setBaseURI(string calldata _baseURI_) external onlyOwnerOrManager {
+//     baseURI = _baseURI_;
+//   }
+//   function setUnrevealedURI(string calldata uri_) public onlyOwnerOrManager {
+//     unrevealedTokenUri = uri_;
+//   }
+//   function setReveal(bool bool_) external onlyOwnerOrManager {
+//     revealed = bool_;
+//   }
